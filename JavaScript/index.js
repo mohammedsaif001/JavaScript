8fa43612deec5f1883dd8fdf9a927230de6960ab
Mohammed Saif <mdsaif4online@gmail.com>
2022-01-18 23:42:43 +0530

    consoleJS

---
+++ JavaScript/index.js
@@ -0,0 +1,11 @@
+console.time("Your Code Took");
+console.log("Hello Villain");
+console.log(4+34);
+console.log([34,45,67]);
+console.log({villain:"saif",age:'21',cgpa:'9.1'});
+console.table({villain:"saif",age:'21',cgpa:'9.1'});
+console.warn("This is a Warning");
+// console.clear();
+console.timeEnd("Your Code Took");
+console.assert(789<190,'Age>190 isnt possible');
+console.error("Error Here!!");
